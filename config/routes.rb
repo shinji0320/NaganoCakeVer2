1c88d718ba488684ee0ea5a86d90831e8376e889
shinji0320 <shinji.blue.one@gmail.com>
2021-01-19 07:01:50 +0900

    顧客側ヘッダー作成,トップ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
- devise_for :customers
+  devise_for :customers
+  root to: 'homes#top'
+  get 'homes/about'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
