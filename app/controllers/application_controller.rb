db9211975265ed51e28217af2349f90acc83f5ee
shinji0320 <shinji.blue.one@gmail.com>
2021-01-19 17:51:07 +0900

    管理者￥

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  def after_sign_in_path_for(resource)
+    get 'homes/about'
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
   devise_for :customers
   namespace :public do
     root to: 'homes#top'
     get 'homes/about'
+  end
+
+  namespace :admin do
+    resources :items
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:index, :show, :update]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
