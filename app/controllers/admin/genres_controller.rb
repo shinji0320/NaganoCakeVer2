730714bb2b734b5e128dfed4ad03cccfc2c0c5c2
EC2 Default User <ec2-user@ip-172-31-40-20.ap-northeast-1.compute.internal>
2021-01-21 01:35:34 +0000

    ジャンル、商品、会員のcontorollerとview

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,34 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to
+    else
+      render :
+    end
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:success] = "ジャンルを変更しました"
+      redirect_to 
+    else
+      render :
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name, :is_valid)
   end
 end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,40 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new
   end
 
   def index
+    @items = Item.all
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to 
+    else
+      render :
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    if @item.update(item_params)
+      flash[:success] = "商品内容をを変更しました"
+      redirect_to 
+    else
+      render :
+    end
+  end
+  
+  private
+  def item_params
+    params.require(:product).permit(:name, :image, :explanation, :genre_id, :tax_out_price, :is_sale)
   end
 end
