c3fe4acf7dbc753e461b1092a33158e5118f04b0
EC2 Default User <ec2-user@ip-172-31-40-20.ap-northeast-1.compute.internal>
2021-01-22 01:53:50 +0000

    update

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,14 +1,16 @@
 class Admin::GenresController < ApplicationController
   def index
     @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
     @genre = Genre.new(genre_params)
     if @genre.save
-      redirect_to
+      flash[:notice] = "ジャンルを追加しました"
+      redirect_to admin_genres_path
     else
-      render :
+      render :index
     end
   end
 
@@ -20,9 +22,9 @@ class Admin::GenresController < ApplicationController
     @genre = Genre.find(params[:id])
     if @genre.update(genre_params)
       flash[:success] = "ジャンルを変更しました"
-      redirect_to 
+      redirect_to admin_genres_path
     else
-      render :
+      render :edit
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :installs
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
 
