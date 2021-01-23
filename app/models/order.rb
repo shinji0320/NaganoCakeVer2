8bfcd51cc4b126ffd2b5efc6ebb887d71e11511c
Sugai-Ayano <b3w41067a@gmail.com>
2021-01-23 01:02:06 +0000

    enumとbootstrap追加、

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,8 @@ class Order < ApplicationRecord
               :name, :shipping_cost, :total_price, presence: true
     belongs_to :customer
     has_many :order_items
-    Order.all.sum(:subtotal)
+    # Order.all.sum(:subtotal)
+    attachment :image
+    enum status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送確認: 3, 発送済み: 4 }
+
 end
