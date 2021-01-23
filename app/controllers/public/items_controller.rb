13fb6ec1e6bb2dc9444b4ebbb6c5f63cc8376f04
EC2 Default User <ec2-user@ip-172-31-88-198.ec2.internal>
2021-01-23 07:28:24 +0000

    [pull]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,13 +1,13 @@
 class Public::ItemsController < ApplicationController
 
-  def index
-    @items = Item.Where(is_sold: true).order("id desc")
-    # @items = Item.Where(is_sold: true).order("id desc").page(params[:page]).per(8)
-  end
+  def index
+    @items = Item.where(is_sold: true).order("id desc")
+    # @items = Item.Where(is_sold: true).order("id desc").page(params[:page]).per(8)
+  end
 
-  def show
-    @item = Item.find(params[:id])
-    @cart_item = Cart_item.new
-  end
+  def show
+    @item = Item.find(params[:id])
+    @cart_item = Cart_item.new
+  end
 
 end
