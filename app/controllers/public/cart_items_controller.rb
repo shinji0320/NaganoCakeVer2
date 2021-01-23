b25133bca0bbdaabce17339ca7f83ca01f34c5d3
EC2 Default User <ec2-user@ip-172-31-88-198.ec2.internal>
2021-01-23 09:16:26 +0000

    [pull]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,16 +4,6 @@ class Public::CartItemsController < ApplicationController
     @cart_items = Cart_item.where(customer_id: current_customer.id)
   end
 
-<<<<<<< HEAD
-  def create
-    cart_item = Cart_item.new(cart_item_params)
-    if cart_item.save
-      redirect_to cart_items_path(cart_item)
-    else
-      render "index"
-    end
-  end
-=======
   def create # カートに入れるアクション
     @cart_item = current_customer.cart_item.build(item_id: params[:item_id]) if @cart_item.blank
     @cart_item.count += params[:count].to_i
@@ -23,17 +13,15 @@ class Public::CartItemsController < ApplicationController
       redirect_to contolloer: "public::items", action: "show"
     end
   end
-  
-  def update # カート内個数の変更
-    
->>>>>>> 3730b492e05b66977c1f5cef9fc707c040597b14
+
+  # def update # カート内個数の変更
 
   def empty_cart
     @cart_items = Cart_item.current_customer.id
   end
 
   private
-  
+
     def set_ca_item
     @line_item = current_cart.line_items.find_by(product_id: params[:product_id])
     end
