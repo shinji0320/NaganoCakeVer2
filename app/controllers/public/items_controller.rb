9bd987930f5b9d792f6ab3fb2818e8728a529e91
EC2 Default User <ec2-user@ip-172-31-40-20.ap-northeast-1.compute.internal>
2021-01-23 07:11:03 +0000

    商品一覧と商品詳細

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,12 @@
 class Public::ItemsController < ApplicationController
 
   def index
+    @items = Item.where(is_sold: true).page(params[:page]).per(4)
   end
 
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 
 end
