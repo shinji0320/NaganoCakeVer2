67f062ec53546fd7298f6d6c813036605ac10999
Sugai-Ayano <b3w41067a@gmail.com>
2021-01-23 07:33:41 +0000

    update

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
     belongs_to :customer
     has_many :order_items
-    # Order.all.sum(:subtotal)
+    Order.all.sum(:subtotal)
     attachment :image
     enum status: { "入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4 }
     validates :customer_id, :payment_method, :status, :address, :postal_code,
