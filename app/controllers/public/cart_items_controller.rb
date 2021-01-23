4b464953ab8b439e799042b97bfa439fa8ecda17
TOMOKIHAGUI <higosozan18@gmail.com>
2021-01-23 09:18:35 +0000

    カート作成中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -13,16 +13,17 @@ class Public::CartItemsController < ApplicationController
       redirect_to contolloer: "public::items", action: "show"
     end
   end
-  
+
   def update # カート内個数の変更
-    
+  end
+
 
   def empty_cart
     @cart_items = Cart_item.current_customer.id
   end
 
   private
-  
+
     def set_ca_item
     @line_item = current_cart.line_items.find_by(product_id: params[:product_id])
     end
@@ -30,5 +31,4 @@ class Public::CartItemsController < ApplicationController
     def cart_item_params
      params.require(:cart_item).permit(:customer_id, :item_id, :count)
     end
-
 end
