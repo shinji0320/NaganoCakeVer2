3285474f7107fc39f790b09a7fbcfee7122f2de2
EC2 Default User <ec2-user@ip-172-31-88-198.ec2.internal>
2021-01-23 04:09:20 +0000

    [Add] controllers

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,18 +1,44 @@
+# 20210122なぜ警告マークが表示されるのかわからない
 class Public::CartItemsController < ApplicationController
 
-  def index
-  end
+  def index
 
-  def create
-  end
+    @cart_items = Cart_item.where(params[:customer_id])
 
-  def update
-  end
+  end
 
-  def destroy
-  end
+  def create
+      cart_item = Cart_item.new(cart_item_params)
+      if cart_item.save
+        redirect_to cart_items_path(cart_item)
+      else
+        render "index"
+      end
+  end
 
-  def empty_cart
-  end
+  def update
+      cart_item = Cart_item.find(params[:id])
+      if cart_item.update(cart_item_params)
+        redirect_to cart_items_path(cart_item.customer_id)
+      else
+        @cart_items =Cart_item.where(params[:customer_id])
+        flash[:danger] = "Invalid errors."
+        render "index"
+      end
+  end
+
+  def destroy
+      cart_item = Cart_item.find(params[:id]).destroy
+      redirect_back(fallback_location: root_path)
+  end
+
+  def empty_cart
+    @cart_items = Cart_items.current_customer.id
+  end
+
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:customer_id, :item_id, :count)
+  end
 
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,3 +1,7 @@
 class CartItem < ApplicationRecord
   validates :customer_id, :item_id, :count, presence: true
+
+  def price_with_tax(price)
+    return (item_price * tax).to_i
+  end
 end
