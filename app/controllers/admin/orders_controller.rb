4dc1a8f4b2b97edb8fe159519162b91024e842b3
shinji0320 <shinji.blue.one@gmail.com>
2021-01-23 16:04:34 +0900

    fix

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,7 +10,16 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order.update
-    redirect_to admin_order_path(@order)
+    if @order.update(making_status_params)
+      flash[:success] = "をを変更しました"
+      redirect_to admin_order_path(@order)
+    else
+      render :show
+    end
+  end
+
+  private
+  def making_status_params
+    params.require(:order).permit(:making_status)
   end
 end
