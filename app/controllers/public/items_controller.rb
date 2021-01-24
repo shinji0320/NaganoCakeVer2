a650a5b2a0ec5d14ad230ce84e44db118abc09db
TOMOKIHAGUI <higosozan18@gmail.com>
2021-01-24 04:47:44 +0000

    add:カート追加機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,8 +6,9 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    #@cart_item = CartItem.new
+    @cart_item = CartItem.new
   end
+
 
   private
 
